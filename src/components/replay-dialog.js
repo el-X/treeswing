3ff087ced275a614f734c3ff368421f9681db376
Alexander Keller <>
2018-10-14 11:11:26 +0200

    Make dialog browser independable

--- src/components/replay-dialog.js
+++ src/components/replay-dialog.js
@@ -2,20 +2,19 @@
 
 export default class ReplayDialog {
   constructor() {
-    this.dialog = document.getElementById("replay-dialog");
+    this.dialog = document.getElementById("myModal");
     this.qrCodeImage = document.getElementById("replay-dialog-qrcode");
     this.linkLabel = document.getElementById("replay-dialog-link");
-    let closeButton = document.getElementById("replay-dialog-close-button");
+    let closeButton = document.getElementsByClassName("close")[0];
 
     closeButton.addEventListener("click", () => {
-      this.dialog.close();
+      this.dialog.style.display = "none";
     });
   }
 
   show(qrCodeAsBase64, replayUrl) {
     this.qrCodeImage.src = qrCodeAsBase64;
     this.linkLabel.innerText = replayUrl;
-    // this.dialog.showModal();
-    alert("Replay URL: " + replayUrl);
+    this.dialog.style.display = "block";
   }
 }
